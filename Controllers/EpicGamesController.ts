4be459c710d19b6311b2bfdbb80a3a4503e63dae
ajneelansh <ajneelansh@gmail.com>
2024-12-31 18:06:12 +0530

    Updated Fortnite Client

--- Controllers/EpicGamesController.ts
+++ Controllers/EpicGamesController.ts
@@ -1,17 +1,93 @@
 import {Request, Response} from 'express'
 import axios from 'axios'
+import { AxiosError } from 'axios';
 import getAccessToken from '../helpers/EpicClient'
 
 const access_token = getAccessToken();
 
-const getAccountId = async (username:string) => {
+const getAccountId = async (username:string):Promise<string>=> {
   const response = await axios.get('https://api.epicgames.dev/users/public/names?displayName=${username}',{
     headers:{Authorization:'Bearer ${access_token}'}
   });
   return response.data[0]?.id;
 }
 
-export const fortniteFetchMatchStat = async(req:Request, res:Response): Promise<any> => {
+const getRecentMatches = async (accountId:string): Promise<any[]> => {
+  try{
+    const response = await axios.get('https://api.fortnitetracker.com/v1/profile/account/${accountId}',{
+       headers: {'TRN-Api-Key': process.env.TRN_APIKEY}
+    });
     
+    const matches = response.data.matches;
+    return matches.slice(0,5);
+  }
+  catch(err:unknown){
+    if (err instanceof AxiosError) {
+      throw new Error(`Cannot get Recent Matches: ${err.message}`);
+    } else {
+      throw new Error('An unknown error occurred');
+    }
+  }
+}
 
+const filterMatches = async (
+  matches: any[],
+  criteria :{
+    matchType : string;
+    accountIds : string[];
+  }
+) =>{
+ try{
+  return matches.find((match) => {
+    if (match.playlist !== criteria.matchType) {
+      return false;
+    }
+    const playerAccountIds = match.players.map((player:any)=> player.id);
+    return criteria.accountIds.every((id:string) => playerAccountIds.includes(id)); 
+    }
+  )
+ }
+ catch(err:unknown){
+  if (err instanceof AxiosError) {
+    throw new Error(`No Matches Found: ${err.message}`);
+  } else {
+    throw new Error('An unknown error occurred');
+  } 
+ }
+}
+
+
+export const fortniteFetchMatchStat = async(req:Request, res:Response): Promise<any> => {
+  try{    
+   const username = req.query.username as string;
+   const matchType = req.query.matchType as string;
+   const accountIds = (req.query.accountIds as string)?.split(',');
+   const criteria = { matchType, accountIds };
+
+   if(!username) {
+    return res.status(400).json({message:"username is required"})
+   }
+
+   const accountId = await getAccountId(username);
+   const matches = await getRecentMatches(accountId);
+
+   const match = filterMatches(matches , criteria);
+   
+   if (match) {
+    return res.json({
+      message: "Match Details Fetches Sucessfully",
+      matchDetails : match,
+    });
+  } else {
+    return res.status(404).json({ message: "No match found for the given criteria" });
+  }
+
+  }
+  catch(err:unknown){
+    if (err instanceof AxiosError) {
+      throw new Error(`No Matches Found: ${err.message}`);
+    } else {
+      throw new Error('An unknown error occurred');
+    } 
+   }
 }
